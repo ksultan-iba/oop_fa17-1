775082bd4817288ace9db5e41e5927448c174f71
IBAPK\12627 <mohammedzayn97@hotmail.com>
2017-09-07 04:36:16 -0700

    question 1 of assaginment

---
+++ q1/src/com/company/q1.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+public class q1 {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner key = new Scanner (System.in);
+
+        System.out.println("enter the number (must be greater then 0)");
+        int n = key.nextInt();
+        int sum=0;
+        for(int i=1 ; i<=n ; i++){
+            sum = sum + i ;
+
+
+
+        }
+        System.out.println("your sum is: "+sum);
+    }
+
+}
+
+
